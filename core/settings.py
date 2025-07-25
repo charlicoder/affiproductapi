a4644e260ac54d1d40b8580fa3a13dcac6cc265f
Khondoker Md Mamunur Rashid <charlicoder@gmail.com>
2025-07-26 01:48:47 +0300

    Update cors

--- core/settings.py
+++ core/settings.py
@@ -73,21 +73,14 @@ DATABASES = {
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db/db1111.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
     }
 }
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": config("DB_NAME"),
-#         "USER": config("DB_USER"),
-#         "PASSWORD": config("DB_PASSWORD"),
-#         "HOST": config("DB_HOST"),
-#         "PORT": config("DB_PORT"),
-#     }
-# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
